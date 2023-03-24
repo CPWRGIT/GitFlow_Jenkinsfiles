bc87ce6db59b8cfe05a0bbfb182e0f0bf48f6b24
Ralph <ralph_nuesse@bmc.com>
2023-03-24 10:06:32 +0100

    Add Task Transfer script

---
+++ vars/WorkFlow_TransferTasksToRelease.groovy
@@ -0,0 +1,29 @@
+currentBuild.displayName = "Xfer 2 Release ${IspwApplication} - ${ReleaseId}"
+
+def hostConnectionCwc2  = '38e854b0-f7d3-4a8f-bf31-2d8bfac3dbd4' 
+def hostConnectionCwcc  = ''
+
+def hotConnection = hostConnectionCwc2
+
+node{
+    dir(".\\") 
+    {
+        deleteDir()
+    }
+
+    stage("Release"){
+
+        ispwOperation(
+            connectionId:               hostConnection, 
+            credentialsId:              CesCredentials,
+            consoleLogResponseBody:     true, 
+            ispwAction:                 'TransferTask', 
+            ispwRequestBody:            """runtimeConfiguration=${IspwRuntimeConfig}
+                                            assignmentId=${AssignmentId}
+                                            level=RLSE
+                                            containerId=${ReleaseId}
+                                            containerType=R"""
+        )        
+    }
+    
+}
