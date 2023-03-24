edee775b4c499906640b3e7aa459e6dbbcdee3d7
Ralph <ralph_nuesse@bmc.com>
2023-03-24 12:48:04 +0100

    Env Settings

--- vars/GitFlow_Main.groovy
+++ vars/GitFlow_Main.groovy
@@ -5,9 +5,12 @@ def releaseAssignmentId
 def ispwOwner
 def cesToken
 def xlrReleaseNumber
-def envSettings = [:]
+
 
 def call(Map runtimeParms) {
+    
+    def envSettings = [:]
+    
     node {
         
         envSettings['hostConnection']        = '38e854b0-f7d3-4a8f-bf31-2d8bfac3dbd4'
