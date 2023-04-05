f60927c259df6f4881375075e74ada18779fa35a
Ralph <ralph_nuesse@bmc.com>
2023-04-05 09:31:43 +0200

    Close Release

--- vars/WorkFlow_CloseRelease.groovy
+++ vars/WorkFlow_CloseRelease.groovy
@@ -1,89 +1,24 @@
 currentBuild.displayName = "Closing ${ispwApplication} - Release ${releaseId}"
 
+def hostConnectionCwc2  = '38e854b0-f7d3-4a8f-bf31-2d8bfac3dbd4' 
+def hostConnectionCwcc  = ''
+
+def hostConnection      = hostConnectionCwc2
+
 node{
 
     cleanWs()
     
-    stage("Clone Repo"){
-        
-        checkout(
-            changelog: false, 
-            poll: false, 
-            scm: [
-                $class: 'GitSCM', 
-                branches: [[name: '*/main']], 
-                doGenerateSubmoduleConfigurations: false, 
-                extensions: [], 
-                submoduleCfg: [], 
-                userRemoteConfigs: [
-                    [
-                        credentialsId: GitCredentials, 
-                        url: "https://github.com/CPWRGIT/${gitRepository}.git"
-                    ]
-                ]
-            ]
-        )
-    }
-    
-    stage("Add Tag"){
-        
-        def gitMessage = '"Release ' + tagName + '"'
-        
-        def stdOut = bat(
-            label: '', 
-            returnStdout: true, 
-            script: 'git tag -a ' + tagName + ' -m ' + gitMessage
-        )
-        
-        echo stdOut
-        
-        withCredentials(
-            [
-                usernamePassword(
-                    credentialsId:      GitCredentials, 
-                    passwordVariable:   'GitHubPassword', 
-                    usernameVariable:   'GitHubUser'
-                )
-            ]
-        ) 
-        {
-
-            stdOut = bat(
-                returnStdout: true, 
-                script: 'git remote set-url origin https://' + GitHubUser + ':' + GitHubPassword + "@github.com/CPWRGIT/${gitRepository}.git"
-            )
-
-            echo stdOut
-
-            stdOut = bat(
-                returnStdout: true, 
-                script: "git push origin ${tagName}"
-            )
-    
-            echo stdOut
-            
-        }
-    }
-}
-
-currentBuild.displayName = "Close Release ${IspwApplication} - ${ReleaseId}"
-
-node{
-    dir(".\\") 
-    {
-        deleteDir()
-    }
-
-    stage("Release"){
+    stage("Close Release"){
 
         ispwOperation(
-            connectionId:           '196de681-04d7-4170-824f-09a5457c5cda', 
-            credentialsId:          CesCredentials,
+            connectionId:           hostConnection, 
+            credentialsId:          cesCredentials,
             consoleLogResponseBody: true, 
             ispwAction:             'CloseRelease', 
             ispwRequestBody: """
-                runtimeConfiguration=ispw
-                releaseId=${ReleaseId}"""
+                runtimeConfiguration=${ispwRuntimeConfig}
+                releaseId=${releaseId}"""
         )
     }
     
