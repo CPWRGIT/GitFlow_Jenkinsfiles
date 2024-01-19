bbacbf8ee498b366f3520abfdfbee0a382534ea5
Ralph <ralph_nuesse@bmc.com>
2024-01-19 11:19:33 +0100

    Reverse

--- vars/GitFlow_Main.groovy
+++ vars/GitFlow_Main.groovy
@@ -249,20 +249,6 @@ def cloneRepo(settings) {
 
     stage ('Checkout') {
 
-        checkout(
-            changelog:  false, 
-            poll:       false, 
-            scm:        [
-                $class:             'GitSCM', 
-                branches:           [[name: BRANCH_NAME]], 
-                extensions:         [], 
-                userRemoteConfigs:  [[
-                    credentialsId:  settings.git.credentialsId, 
-                    url:            settings.git.repoUrl
-                ]]
-            ]
-        )
-/*
         if(BRANCH_NAME.contains("release")) {
 
             checkout(
@@ -285,7 +271,6 @@ def cloneRepo(settings) {
             checkout scm
         }
     }
-*/
 }
 
 def extractCredentials(credentialsId) {
